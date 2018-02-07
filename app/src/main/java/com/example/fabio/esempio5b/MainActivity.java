e224eb8f88ae91905b5bfd56071a6b74e4ebffb1
fabio biscaro <fabio.biscaro@gmail.com>
2018-02-07 16:15:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fabio/esempio5b/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.fabio.esempio5b;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Recupero il puntatore all'elemento grafico
+        button = (Button) findViewById(R.id.grecoButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Boh","tasto premuto");
+                Toast t=  Toast.makeText(getApplicationContext(),"Tasto premuto",Toast.LENGTH_LONG);
+                t.show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        System.out.println("ciao");
+    }
+}
